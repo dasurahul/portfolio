dfcf24489edd00ecd5daebdd0bc820b3d72a2f94
dasurahul <dasurahul598@gmail.com>
2022-02-03 19:49:40 +0530

    Added app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const name = "Rahul";
+const fullName = "Dasu Rahul";
+const dateOfBirth = "14 Dec, 2001";
+const address = "Bhanjanagar, Odisha";
+const pinCode = "761126";
+const emailAddress = "dasurahul598@gmail.com";
+const phoneNumber = "+91 9337 086 565";
+
+document.querySelector(".logo div").innerHTML = name;
+document.querySelector(".name").innerHTML = name;
+document.querySelector(".full-name").innerHTML = fullName;
+document.querySelector(".date-of-birth").innerHTML = dateOfBirth;
+document.querySelector(".address").innerHTML = address;
+document.querySelector(".pin-code").innerHTML = pinCode;
+document.querySelector(".email-address").innerHTML = emailAddress;
+document.querySelector(".phone-number").innerHTML = phoneNumber;
