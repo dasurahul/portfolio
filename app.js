1dbf393322b1e19b0c412617b140f78be2dba20e
dasurahul <dasurahul598@gmail.com>
2022-02-04 10:13:55 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const address = "Bhanjanagar, Odisha";
 const pinCode = "761126";
 const emailAddress = "dasurahul598@gmail.com";
 const phoneNumber = "+91 9337 086 565";
+const educations = [
+  {
+    year: "2019 - 2022",
+    name: "Bachelor of Computer Application",
+    university: "Berhampur University",
+  },
+];
+
 const linkedInProfileUrl = "https://www.linkedin.com/in/rahul-dasu-3863b9218";
 const githubProfileUrl = "https://www.github.com/dasurahul";
 const quotation =
@@ -18,6 +26,7 @@ const addressPlace = document.querySelectorAll(".address");
 document.querySelector(".pin-code").innerHTML = pinCode;
 const emailAddressPlace = document.querySelectorAll(".email-address");
 const phoneNumberPlace = document.querySelectorAll(".phone-number");
+const educationsContainer = document.querySelector(".educations");
 document.querySelector(".linkedin").href = linkedInProfileUrl;
 document.querySelector(".github").href = githubProfileUrl;
 document.querySelector(".quotation p").innerHTML = quotation;
@@ -30,10 +39,24 @@ addressPlace.forEach((eachPlace) => {
   eachPlace.innerHTML = address;
 });
 
+emailAddressPlace.forEach((eachPlace) => {
+  eachPlace.innerHTML = emailAddress;
+});
+
 phoneNumberPlace.forEach((eachPlace) => {
   eachPlace.innerHTML = phoneNumber;
 });
 
-emailAddressPlace.forEach((eachPlace) => {
-  eachPlace.innerHTML = emailAddress;
+educations.forEach((education) => {
+  const educationDiv = document.createElement("div");
+  educationDiv.classList.add("education");
+  const yearDiv = document.createElement("div");
+  yearDiv.innerHTML = "<strong>" + education.year + "</strong>";
+  const nameDiv = document.createElement("div");
+  nameDiv.innerHTML = "<strong>" + education.name + "</strong>";
+  const universityDiv = document.createElement("div");
+  universityDiv.classList.add("gray");
+  universityDiv.innerHTML = education.university;
+  educationDiv.append(yearDiv, nameDiv, universityDiv);
+  educationsContainer.append(educationDiv);
 });
