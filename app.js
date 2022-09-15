f349e4334a25d592076f60cb42b768a79483950c
dasurahul <dasurahul598@gmail.com>
2022-09-15 23:41:09 +0530

    added description

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const fullName = "Dasu Rahul";
 const designation = "Software Engineer";
 const descriptions = [
   "I’m living the dream.",
-  "I’ve always been a great problem solver, an independent introvert, and a technophile obsessed with the latest devices. Today, I’m working from home as a software engineer for Kellton, and I get to show off all these elements of who I am.",
+  "I’ve always been a great problem solver, an independent introvert, and a technophile obsessed with the latest devices. Today, I’m working from home as a software engineer, and I get to show off all these elements of who I am.",
   "I’m always adding new skills to my repertoire. I’m also eager to meet other software engineers in the area, so feel free to connect!",
 ];
 const socialLinks = [
